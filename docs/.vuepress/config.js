ccd55147114f074a3636abbd989571c7acf8b3f4
sunxianfu <1102654482@qq.com>
2021-08-17 17:49:23 +0800

    '增加配置'

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,4 +1,12 @@
 module.exports = {
     title: '先富',
-    description: 'Just playing around'
-}
+    description: 'Just playing around',
+    themeConfig: {
+        nav: [
+            { text: 'Home', link: '/' },
+            { text: 'Guide', link: '/guide/' },
+            { text: 'External', link: 'https://google.com' },
+        ],
+        sidebar: 'auto',
+    },
+};
