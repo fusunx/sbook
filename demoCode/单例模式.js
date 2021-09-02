c79779138c311724fdad490588e488309ff81c04
sunxianfu <1102654482@qq.com>
2021-09-02 19:32:44 +0800

    单例模式文章

---
+++ demoCode/单例模式.js
@@ -0,0 +1,47 @@
+// var Singleton = function (name) {
+//     this.name = name
+// }
+
+// Singleton.prototype.getName = function () {
+//     return this.name
+// }
+
+// Singleton.getInstance = (function () {
+//     var instance = null
+//     return function (name) {
+//         if (!instance) {
+//             instance = new Singleton(name)
+//         }
+//         return instance
+//     }
+// })
+
+// var Singleton = (function () {
+//     var instance = null
+//     var Singleton = function (name) {
+//         if (instance) {
+//             return instance
+//         }
+//         this.name = name
+//         return instance = this
+//     }
+//     return Singleton
+// })()
+
+var Singleton = function (name) {
+    this.name = name
+}
+
+var ProxySingleton = (function () {
+    var instance = null
+    return function (name) {
+        if (!instance) {
+            instance = new Singleton(name)
+        }
+        return instance
+    }
+})()
+var a = new ProxySingleton('instance1');
+var b = new ProxySingleton('instance2');
+
+console.log(a === b); // true
