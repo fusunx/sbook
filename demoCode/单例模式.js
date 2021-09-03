3c58dfe6ba4653394a28e7e963e23f2d2dfbfbd7
sunxianfu <1102654482@qq.com>
2021-09-03 19:34:17 +0800

    双向绑定文章

--- demoCode/单例模式.js
+++ demoCode/单例模式.js
@@ -28,20 +28,30 @@
 //     return Singleton
 // })()
 
-var Singleton = function (name) {
-    this.name = name
-}
+// var Singleton = function (name) {
+//     this.name = name
+// }
 
-var ProxySingleton = (function () {
-    var instance = null
-    return function (name) {
-        if (!instance) {
-            instance = new Singleton(name)
-        }
-        return instance
+// var ProxySingleton = (function () {
+//     var instance = null
+//     return function (name) {
+//         if (!instance) {
+//             instance = new Singleton(name)
+//         }
+//         return instance
+//     }
+// })()
+// var a = new ProxySingleton('instance1');
+// var b = new ProxySingleton('instance2');
+
+// console.log(a === b); // true
+
+class Demo {
+    foo = 1;
+    constructor(bar) {
+        this.bar = bar;
     }
-})()
-var a = new ProxySingleton('instance1');
-var b = new ProxySingleton('instance2');
-
-console.log(a === b); // true
+}
+const int = new Demo('aaa');
+Object.assign(int, { foo: 2 });
+console.log(int);
