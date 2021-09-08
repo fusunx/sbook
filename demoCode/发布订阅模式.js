b68d4df2a63f2dd7076aca29044baa6612510a71
sunxianfu <1102654482@qq.com>
2021-09-08 17:31:59 +0800

    发布订阅模式

---
+++ demoCode/发布订阅模式.js
@@ -0,0 +1,39 @@
+let clientList = {};
+
+function myAddEventListener(key, fn) {
+    if (clientList[key]) {
+        clientList[key].push(fn);
+    } else {
+        clientList[key] = [fn];
+    }
+
+    return function unsubscribe() {
+        const index = clientList[key].indexOf(fn);
+        clientList[key].splice(index, 1);
+    };
+}
+
+function myEventTrigger(key) {
+    if (!clientList[key]) {
+        // key 不存在
+        return;
+    }
+
+    clientList[key].forEach((fn) => {
+        fn();
+    });
+}
+
+// 测试
+
+const unsubscribe1 = myAddEventListener('click', () => {
+    console.log(1);
+});
+const unsubscribe2 = myAddEventListener('click', () => {
+    console.log(2);
+});
+const unsubscribe3 = myAddEventListener('click', () => {
+    console.log(3);
+});
+unsubscribe2();
+myEventTrigger('click'); // 1, 2
